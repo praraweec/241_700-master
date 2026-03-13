49e57774e47507d8f448c6d36a209cf0efaf4385
praraweec <prarawee.c@ku.th>
2026-03-13 11:02:56 +0700

    13.3

--- Frontend/user.js
+++ Frontend/user.js
@@ -11,7 +11,28 @@ window.onload = async() => {
 
     let htmlData = '<div>'
     htmlData += '</div>'
-
+      for (let i = 0; i < response.data.length; i++) {
+        let user = response.data[i];
+        htmlData += ` <div>
+        ${user.firstname} ${user.lastname}
+        <button>Edit</button>
+        <button class='delete' data-id='${user.id}'>Delete</button>
+        </div>`
+    }
+    htmlData += '</div>';
     userDOM.innerHTML = htmlData;
 
+    const deleteDOMs = document.getElementsByClassName("delete");
+    for (let i = 0; i < deleteDOMs.length; i++) {
+        deleteDOMs[i].addEventListener("click", async (event) => {
+            // ดึง id ของ user ที่ต้องการลบจาก data-id attribute
+            const id = event.target.dataset.id;
+            try{
+                await axios.delete(`${BASE_URL}/users/${id}`);
+                loadData(); // โหลดข้อมูลใหม่หลังจากลบสำเร็จ
+            }catch(error){
+                console.error("Error deleting user:", error);
+            }    
+        });
+    }
 }
