9db00ced85322c51d2e92c1f7bcae3d0dadaf343
praraweec <prarawee.c@ku.th>
2026-03-13 11:37:26 +0700

    add user.js

--- Frontend/user.js
+++ Frontend/user.js
@@ -1,17 +1,17 @@
 //โหลด user ทั้งหมดจาก api http://localhost:8000/users
 
 //นำ  user z ที่ได้มาแสดงในตาราง
-const BASE_URL = 'http://localhost:8000';
+const BASE_URL = "http://localhost:8000";
+window.onload = async () => {
+    await loadData();
+}
 
-window.onload = async() => {
+const loadData = async () => {
     const response = await axios.get(`${BASE_URL}/users`);
     console.log(response.data);
-
-    const userDOM = document.getElementById('user');
-
-    let htmlData = '<div>'
-    htmlData += '</div>'
-      for (let i = 0; i < response.data.length; i++) {
+    const userDOM = document.getElementById("user");
+    let htmlData = '<div>';
+    for (let i = 0; i < response.data.length; i++) {
         let user = response.data[i];
         htmlData += ` <div>
         ${user.firstname} ${user.lastname}
